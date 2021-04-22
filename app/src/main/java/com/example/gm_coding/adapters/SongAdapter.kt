7603a12d56738d60eeb553f22649ccb9ac25ecd9
Muammar Johnson <muammarj@gmail.com>
2021-04-22 01:28:46 -0400

    adapter created

---
+++ app/src/main/java/com/example/gm_coding/adapters/SongAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.gm_coding.adapters
+
+import android.content.ContentValues
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gm_coding.R
+import com.example.gm_coding.model.Result
+
+class SongAdapter(private val results: List<Result>) :RecyclerView.Adapter<SongAdapter.ViewHolder>(){
+    private  val TAG = "SongAdapter"
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val artistNameTv: TextView
+        val trackNameTv: TextView
+        val releaseDateTv: TextView
+        val primaryGenreNameTv: TextView
+        val trackPriceTv: TextView
+
+        init {
+            artistNameTv = view.findViewById(R.id.artist_name_tv)
+            trackNameTv = view.findViewById(R.id.track_name_tv)
+            releaseDateTv = view.findViewById(R.id.release_date_tv)
+            primaryGenreNameTv = view.findViewById(R.id.primary_genre_name_tv)
+            trackPriceTv = view.findViewById(R.id.track_price_tv)
+                trackPriceTv.textSize = 20F
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.song_item,
+            parent,
+            false
+        )
+        return  ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.trackPriceTv.text = results[position].collectionPrice.toString()
+        holder.trackNameTv.text = results[position].trackName
+       holder.artistNameTv.text = results[position].artistName
+        holder.releaseDateTv.text = results[position].releaseDate
+        holder.primaryGenreNameTv.text = results[position].primaryGenreName
+    }
+
+    override fun getItemCount(): Int {
+        return results.size
+    }
+}
